62e0837296d80562472a55c86c414d8c067ebf6b
matiscakosky <scakosky@hotmail.com>
2020-05-24 13:27:19 -0300

    excercise 4 resolved check

--- src/exercise4.clj
+++ src/exercise4.clj
@@ -3,9 +3,5 @@
 (defn summary
   "Calculates the sum of all the values in the sequence of numbers given as input. Use the reduce function."
   [x]
-  (throw (Exception. "Not implemented."))
+  (reduce + x)
 )
-
-
-
-
