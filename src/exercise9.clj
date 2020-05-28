58525c65071d059fcff4651a891f3b3d14ee5b8a
matiscakosky <scakosky@hotmail.com>
2020-05-28 14:50:26 -0300

    excercise 9 resolved check

--- src/exercise9.clj
+++ src/exercise9.clj
@@ -1,11 +1,13 @@
 (ns exercise9)
 
-(defn word-count 
+(defn word-count
   "Returns a map where each key is a distinct word in a given sentence and each value is the number of occurences of that word in the sentence."
   [x]
-  (throw (Exception. "Not implemented."))
+    (frequencies
+    (clojure.string/split
+      (clojure.string/lower-case
+        (clojure.string/replace x #"[:,!!&@$%^&]" "")) 
+        #"\s+"))
+
+
 )
-
-
-
-
